e9ba15d367128bf579325bfe8726d24d15c7c26e
root <jonathanpullen@gmail.com>
2020-08-08 03:20:53 +0100

    install packages needed for pi

---
+++ pi/setup/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get -y install libmysqlclient-dev mariadb-server mariadb-client
+apt-get -y install apache2 apache2-doc apache2-npm-prefork apache2-utils libexpat1 ssl-cert
+apt-get -y install libapache2-mod-php php
