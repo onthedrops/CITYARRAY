96098fcbe04543f576e324c0e74b85cc9d2a1a08
root <jonathanpullen@gmail.com>
2020-08-15 08:59:27 +0100

    start of input form

--- pi/www/index.php
+++ pi/www/index.php
@@ -2,3 +2,27 @@
 
 	require('header.inc.php');
 
+	// display list of signs
+	
+?>
+<FORM METHOD="POST">
+	<TABLE>
+	<TD>Individual sign</TD><TD>
+		<SELECT ID="selIndividual">
+			<OPTION VALUE="0" SELECTED>None</OPTION>
+<?php
+
+?>
+		</SELECT></TD><TD>Group of signs</TD><TD><SELECT ID="selGroup">
+			<OPTION VALUE="0" SELECTED>None</OPTION>
+<?php
+
+?>
+	</SELECT>
+	</TD></TR>
+	<TR><TD COLSPAN=4><TEXTAREA ID="taMsg" ROWS="10" COLS="60"> </TEXTAREA></TD></TR>
+	<TR><TD></TD><TD></TD><TD></TD><TD ALIGN="RIGHT"><INPUT TYPE="SUBMIT" /></TD></TR>
+	</TABLE>
+	</FORM>
+
+
