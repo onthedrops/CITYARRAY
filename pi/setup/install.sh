57bcf4c9a02fc71c04df1306cfd6b1c4241738ac
root <jonathanpullen@gmail.com>
2020-10-13 07:59:04 +0100

    ..

--- pi/setup/install.sh
+++ pi/setup/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get -y install libmysqlclient-dev mariadb-server mariadb-client
+apt-get -y install mariadb-server mariadb-client
 apt-get -y install apache2 apache2-doc apache2-npm-prefork apache2-utils libexpat1 ssl-cert
 apt-get -y install libapache2-mod-php php
 apt-get -y install php7.3-mysql
