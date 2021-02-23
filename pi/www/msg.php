b85379e936f420918c882a342116abdf07042a4a
Jonathan Pullen <jonathanpullen@gmail.com>
2021-02-23 01:38:52 -0800

    change version that triggers auto update

--- pi/www/msg.php
+++ pi/www/msg.php
@@ -59,7 +59,7 @@
 		$dbh->Query("UPDATE signs SET signVersion = $signVersionq WHERE signId = $signId");
 	}
 
-	if($_REQUEST['ver'] != '0.14') {
+	if($_REQUEST['ver'] != '0.15') {
 		$dbh->Query("SELECT configValue FROM signConfig WHERE signId = $signId AND configKey = 'auto'");
 		$dbh->next_record();
 		$auto = $dbh->f("configValue");
