d7fb4adf3c5cedf9fa1a4cbef07050f7b3808a77
root <jonathanpullen@gmail.com>
2022-07-22 12:18:10 +0100

    ..

--- pi/www/log.php
+++ pi/www/log.php
@@ -2,13 +2,15 @@
 
         require('header.inc.php');
 ?>
-<A HREF=/log.php?file=bt>Bluetooth</A>&nbsp;&nbsp;<A HREF=/log.php?file=web>Web</A>&nbsp;&nbsp;<A HREF=/log.php?file=sign>sign</A>&nbsp;&nbsp;
+<A HREF=/log.php?file=bt>Bluetooth</A>&nbsp;&nbsp;<A HREF=/log.php?file=web>Web</A>&nbsp;&nbsp;<A HREF=/log.php?file=sign>sign</A>&nbsp;&nbsp;<A HREF=/log.php?file=remote>remote</A>&nbsp;&nbsp;<A HREF=/log.php?file=remoteerr>Remote Error</A>
 <PRE>
 <?php
 	include_once('firmware_version.php');
 
 	if(isset($_REQUEST['file'])) {
 		switch($_REQUEST['file']) {
+			case "remote": $file = "/home/sign-firmware/pi/www/remote_support.txt"; break;
+			case "remoteerr": $file = "/home/sign-firmware/pi/www/remote_support.err"; break;
 			case "bt": $file = '/var/log/signbtd.log'; break;
 			case "web": $file = '/var/log/apache2/access.log'; break;
 			default: $file = '/var/log/signd.log'; break;
